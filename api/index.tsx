81321563b0b821618a0ebac590c0af7f072d2c2d
l4rot <dkiani@byte5.de>
2024-05-20 13:27:39 +0200

    basic layout added

--- api/index.tsx
+++ api/index.tsx
@@ -3,65 +3,112 @@ import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
+import { Box, Heading, Text, VStack, vars } from '../ui.js'
+
+type State = {
+  count: number
+}
 
 // Uncomment to use Edge Runtime.
 // export const config = {
 //   runtime: 'edge',
 // }
 
-export const app = new Frog({
+
+export const app = new Frog<{ State: State }>({
+  initialState: {
+      count: 0
+    },
+  ui: { vars },
   assetsPath: '/',
   basePath: '/api',
+  imageAspectRatio: '1:1',
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
 
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+  // const { buttonValue, inputText, status } = c
   return c.res({
     image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
+      <Box
+        grow
+        alignHorizontal="center"
+        backgroundColor="background"
+        padding="32"
       >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
+        <VStack gap="4">
+          <Heading>Start</Heading>
+          <Text color="text200" size="20">
+            Open your Pack
+          </Text>
+        </VStack>
+      </Box>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button action='/tx'>Get Pack(tx)</Button>,
+    ],
+  })
+})
+
+app.frame('/tx', (c) => {
+  return c.res({
+    image: (
+      <Box
+        grow
+        alignHorizontal="center"
+        backgroundColor="background"
+        padding="32"
+      >
+        <VStack gap="4">
+          <Heading>tx</Heading>
+          <Text color="text200" size="20">
+            Preparing your Transaction
+          </Text>
+        </VStack>
+      </Box>
+    ),
+    intents: [
+      <Button action='/unseal'>UNSEAL</Button>,
+      <Button.Link href='https://basescan.org/tx/0xa7f19a029ebb67cd9ea2e247641468bd852af2cd37a2661d924c4613a84b4d54'>view on Blockexplorer</Button.Link>,
+    ],
+  })
+})
+/* 
+start
+card 1 (common)
+card 2 (common)
+...
+card 5 (rare)
+overview
+*/
+app.frame('/unseal', (c) => {
+  const { buttonValue, deriveState } = c
+  const state:any = deriveState(previousState => {
+    if (buttonValue === 'inc') previousState.count++
+    if (buttonValue === 'dec') previousState.count--
+  })
+  return c.res({
+    image: (
+      <Box
+        grow
+        alignHorizontal="center"
+        backgroundColor="background"
+        padding="32"
+      >
+        <VStack gap="4">
+          <Heading>UNSEAL</Heading>
+          <Text color="text200" size="20">
+            Unseal Pack
+            Card: {state.count}
+          </Text>
+        </VStack>
+      </Box>
+    ),
+    intents: [
+      state.count !== 0 && <Button value='dec'>⬅️</Button>,
+      state.count <= 5 && <Button value="inc">➡️</Button>,
+      state.count == 6 && <Button value="inc">GET ANOTHER PACK</Button>,
     ],
   })
 })
